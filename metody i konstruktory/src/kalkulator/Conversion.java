13df6c1a56594a4f8d29c85835b03f5df6dfb45f
oxryg <56882369+oxryg@users.noreply.github.com>
2019-10-22 17:10:17 +0100

    Update Conversion.java

--- metody i konstruktory/src/kalkulator/Conversion.java
+++ metody i konstruktory/src/kalkulator/Conversion.java
@@ -3,34 +3,33 @@ package kalkulator;
 public class Conversion {
 
     public static void main(String[] args) {
-       int hours = 12;
-       int seconds = 30;
-       int minutes = 200;
-
-        double meter = 2.5;
-        double centi = 125;
-        double milli = 1555;
-
+        
+       TimeConversion conversion1 = new TimeConversion();
+        int hours = 12;
+        int minutes = conversion1.houersToMinutes(hours);
+        int seconds = conversion1.minutesToSeconds(minutes);
+        
+        System.out.print(hours + " godzin to ");
+        System.out.println(minutes + " minut");
+        System.out.print( minutes + " minut to ");
+        System.out.println(seconds + " sekund");
+        System.out.print(seconds + " sekund to ");
+        System.out.println(conversion1.secondToMilliseconds(seconds) + " milisekund");
+        System.out.println();
+                
         MeterConversion conversion = new MeterConversion();
+        double meter = 7.35;
+        double centi = conversion.meterToCenti(meter);
+        double milli = conversion.meterToMilli(meter);
+
         System.out.print(meter + "m to ");
-        System.out.println(conversion.meterToCenti(meter)+"cm");
+        System.out.println(centi + "cm");
         System.out.print(meter + "m to ");
-        System.out.println(conversion.meterToMilli(meter) + "mm");
+        System.out.println(milli + "mm");
         System.out.print(centi + "cm to ");
         System.out.println(conversion.centiToMeter(centi) + "m");
         System.out.print(milli + "mm to ");
-        System.out.println(conversion.milliToMeters(milli) + "m");
-        System.out.println();
-        System.out.println();
-
-        TimeConversion conversion1 = new TimeConversion();
-        System.out.print(hours + " godzin to ");
-        System.out.println(conversion1.houersToMinutes(hours) + " minut");
-        System.out.print( minutes + " minut to ");
-        System.out.println(conversion1.minutesToSeconds(minutes) + " sekund");
-        System.out.print(seconds + " sekund to ");
-        System.out.println(conversion1.secondToMilliseconds(seconds) + " milisekund");
-
+        System.out.println(conversion.milliToMeters(milli) + "m");     
 
     }
 }
